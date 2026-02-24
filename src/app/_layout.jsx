496765a2c4b149944e54e3be23245bb747bd3d5a
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-02-24 18:03:51 +0800

    Update root layout

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,30 +1,41 @@
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
 	return (
-		<Stack initialRouteName='profiling'>
-			<Stack.Screen
-				name='profiling'
-				options={{
-					headerShown: false
-				}}
-			/>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<BottomSheetModalProvider>
+				<Stack
+					initialRouteName='profiling'
+					screenOptions={{
+						animation: 'fade',
+						headerBackVisible: false
+					}}
+				>
+					<Stack.Screen
+						name='profiling'
+						options={{
+							headerShown: false
+						}}
+					/>
 
-			<Stack.Screen
-				name='(tabs)'
-				options={{
-					headerTitle: 'Beauwise',
-					headerShadowVisible: false,
-					headerStyle: { backgroundColor: '#fff' }
-				}}
-			/>
-			<Stack.Screen
-				name='scanner'
-				options={{
-					headerShown: false,
-					animation: 'fade'
-				}}
-			/>
-		</Stack>
+					<Stack.Screen
+						name='(tabs)'
+						options={{
+							headerTitle: 'Beauwise',
+							headerShadowVisible: false,
+							headerStyle: { backgroundColor: '#fff' }
+						}}
+					/>
+					<Stack.Screen
+						name='scanner'
+						options={{
+							headerShown: false
+						}}
+					/>
+				</Stack>
+			</BottomSheetModalProvider>
+		</GestureHandlerRootView>
 	);
 }

35efac4f4f03534078342e9d65e4424048178e8d
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-02-24 18:01:16 +0800

    Update MultiChoiceQuestion component

--- src/components/MultiChoiceQuestion.jsx
+++ src/components/MultiChoiceQuestion.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react';
-import { View, Text, Pressable } from 'react-native';
-import Animated, { FadeInRight, FadeOut } from 'react-native-reanimated';
-import Colors from '../constants/Colors';
+import { View, Text } from 'react-native';
 
-export default function MultiCheckBoxContainer({ options, choiceLabel }) {
-	const [active, setActive] = useState([]);
+import { useProfilingStore } from '@/stores/useProfilingStore';
 
-	const handlePress = (id) => () => {
-		setActive((prev) => {
-			if (prev.includes(id)) {
-				return prev.filter((item) => item !== id);
-			} else {
-				return [...prev, id];
-			}
-		});
+import OptionItem from './profiling/OptionItem';
+
+export default function MultiCheckBoxContainer({
+	options,
+	choiceLabel,
+	section,
+	questionIdentifier
+}) {
+	const updateProfile = useProfilingStore((state) => state.setProfile);
+	const currentChoice = useProfilingStore(
+		(state) => state.profile[section][questionIdentifier]
+	);
+
+	const handlePress = (value) => () => {
+		updateProfile(section, questionIdentifier, value);
+	};
+
+	const isSelected = (value) => {
+		return currentChoice.includes(value);
 	};
 
 	return (
-		<View style={{ display: 'flex', rowGap: 8 }}>
+		<View style={{ marginHorizontal: 20 }}>
 			<Text
 				style={{
 					textTransform: 'uppercase',
@@ -29,47 +36,26 @@ export default function MultiCheckBoxContainer({ options, choiceLabel }) {
 				{choiceLabel}
 			</Text>
 
-			<View style={{ display: 'flex', rowGap: 12 }}>
-				{options?.map((item, index) => {
+			<View
+				style={{
+					flexDirection: 'row',
+					flexWrap: 'wrap',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+
+					gap: 12,
+					borderRadius: 20
+				}}
+			>
+				{options?.map((item) => {
 					return (
-						<Pressable onPress={handlePress(item.id)} key={item.id}>
-							<Animated.View
-								entering={FadeInRight.delay(index * 100)}
-								exiting={FadeOut}
-								style={{
-									backgroundColor: active.includes(item.id) ? '#a78bfa49' : '#f8f4f4',
-									borderColor: active.includes(item.id) ? Colors.primary : '#d0d0d0',
-									padding: 12,
-									borderRadius: 20,
-									borderWidth: 1,
-									transitionDuration: 200
-								}}
-							>
-								<Animated.Text
-									style={{
-										color: active.includes(item.id) ? Colors.primary : '#676767',
-										fontSize: 12,
-										fontWeight: '700',
-										transitionDuration: 200
-									}}
-								>
-									{item.label}
-								</Animated.Text>
-
-								{item.description?.length && (
-									<Animated.Text
-										style={{
-											color: active.includes(item.id) ? Colors.primary : '#9f9f9f',
-											fontSize: 10,
-
-											transitionDuration: 200
-										}}
-									>
-										{item?.description}
-									</Animated.Text>
-								)}
-							</Animated.View>
-						</Pressable>
+						<OptionItem
+							key={item.id}
+							item={item}
+							isSelected={isSelected}
+							handlePress={handlePress}
+							section={section}
+						/>
 					);
 				})}
 			</View>

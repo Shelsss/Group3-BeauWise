fbaed6ef8021acb7cd921885c89ff83a10b1b04d
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-02-24 18:00:19 +0800

    Update SingleChoiceQuestion component

--- src/components/SingleChoiceQuestion.jsx
+++ src/components/SingleChoiceQuestion.jsx
@@ -1,66 +1,69 @@
-import { useState } from 'react';
-import { View, Pressable, Text } from 'react-native';
-import Colors from '../constants/Colors';
-import Animated from 'react-native-reanimated';
+import { View, Text } from 'react-native';
 
-export default function SingleChoiceQuestion({ options, choiceLabel }) {
-	const [active, setActive] = useState(0);
+import { useProfilingStore } from '@/stores/useProfilingStore';
+import OptionItem from './profiling/OptionItem';
+export default function SingleChoiceQuestion({
+	options,
+	choiceLabel,
+	section,
+	questionIdentifier,
+	selectedGender,
+	questionId,
+	questionCount
+}) {
+	const updateProfile = useProfilingStore((state) => state.setProfile);
 
-	const handlePress = (id) => () => {
-		setActive(id);
+	const currentChoice = useProfilingStore(
+		(state) => state.profile[section][questionIdentifier]
+	);
+
+	const handlePress = (value) => () => {
+		updateProfile(section, questionIdentifier, value);
+	};
+
+	const isSelected = (value) => {
+		return currentChoice === value;
 	};
 
 	return (
-		<View style={{ display: 'flex', rowGap: 8 }}>
+		<View
+			style={{
+				rowGap: 8,
+				marginHorizontal: 20
+			}}
+		>
 			<Text
 				style={{
 					textTransform: 'uppercase',
-					width: '100%',
-					fontSize: 14,
+					fontSize: 13,
 					fontWeight: '800'
 				}}
 			>
 				{choiceLabel}
 			</Text>
 
-			<View style={{ display: 'flex', rowGap: 12 }}>
+			<View
+				style={{
+					flexDirection: 'row',
+					flexWrap: 'wrap',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+
+					gap: 12,
+					borderRadius: 20
+				}}
+			>
 				{options?.map((item) => {
 					return (
-						<Pressable onPress={handlePress(item.id)} key={item.id}>
-							<Animated.View
-								style={{
-									backgroundColor: active === item.id ? '#a78bfa49' : '#f8f4f4',
-									borderColor: active === item.id ? Colors.primary : '#d0d0d0',
-									padding: 12,
-									borderRadius: 20,
-									borderWidth: 1,
-									transitionDuration: 200
-								}}
-							>
-								<Animated.Text
-									style={{
-										color: active === item.id ? Colors.primary : '#676767',
-										fontSize: 12,
-										fontWeight: '700',
-										transitionDuration: 200
-									}}
-								>
-									{item.label}
-								</Animated.Text>
-
-								{item.description?.length && (
-									<Animated.Text
-										style={{
-											color: active === item.id ? Colors.primary : '#9f9f9f',
-											fontSize: 10,
-											transitionDuration: 200
-										}}
-									>
-										{item.description}
-									</Animated.Text>
-								)}
-							</Animated.View>
-						</Pressable>
+						<OptionItem
+							key={item.id}
+							item={item}
+							isSelected={isSelected}
+							handlePress={handlePress}
+							currentSection={section}
+							selectedGender={selectedGender}
+							questionId={questionId}
+						/>
 					);
 				})}
 			</View>

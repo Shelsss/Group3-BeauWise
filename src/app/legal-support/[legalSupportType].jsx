cf8879bc8a23f092078960b7acf6bb9413614c32
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-04-06 21:46:52 +0800

    Legal route added; update primary button

---
+++ src/app/legal-support/[legalSupportType].jsx
@@ -0,0 +1,180 @@
+import { useLocalSearchParams } from 'expo-router';
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Colors from '@/constants/Colors';
+import TermsOfService from '@/constants/TermsOfService';
+import { Circle } from 'lucide-react-native';
+import PagePadding from '@/constants/PagePadding';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useRef } from 'react';
+import LearnHeader from '@/components/learn/Header';
+import PrivacyPolicy from '@/constants/PrivacyPolicy';
+import MedicalDisclaimer from '@/constants/MedicalDisclaimer';
+import ContactUs from '@/constants/ContactUs';
+import AboutUs from '@/constants/AboutUs';
+import { Image } from 'expo-image';
+
+export default function LegalSupportType() {
+	const { legalSupportType } = useLocalSearchParams();
+	const { bottom } = useSafeAreaInsets();
+	const scrollViewRef = useRef(null);
+	const pageName = legalSupportType
+		.split('-')
+		.map((str) => str.at(0).toUpperCase() + str.split('').splice(1).join(''))
+		.join(' ');
+
+	const displayItem =
+		legalSupportType === 'terms-of-service'
+			? TermsOfService
+			: legalSupportType === 'privacy-policy'
+				? PrivacyPolicy
+				: legalSupportType === 'medical-disclaimer'
+					? MedicalDisclaimer
+					: legalSupportType === 'contact-support'
+						? ContactUs
+						: AboutUs;
+
+	return (
+		<>
+			<LearnHeader title={pageName} />
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				ref={scrollViewRef}
+				onScroll={({ nativeEvent }) => {
+					if (nativeEvent.contentOffset.y < 0) {
+						scrollViewRef.current?.scrollTo({ x: 0, y: 0 });
+					}
+				}}
+				contentContainerStyle={{
+					paddingTop: PagePadding.config.paddingTop,
+					paddingHorizontal: PagePadding.config.paddingHorizontal + 10,
+					paddingBottom: bottom + 20,
+					rowGap: 30
+				}}
+			>
+				{legalSupportType === 'about-us' && (
+					<Image
+						style={{
+							aspectRatio: 16 / 9,
+							width: 200,
+							alignSelf: 'center',
+							marginTop: 20
+						}}
+						source={require('assets/images/logo.webp')}
+					/>
+				)}
+
+				{displayItem.map((item, index) => (
+					<View key={item.title}>
+						<Text style={{ fontWeight: 700, fontSize: 16, color: Colors.textColor }}>
+							{legalSupportType !== 'about-us' &&
+								legalSupportType !== 'contact-support' &&
+								`${index + 1}. `}
+							{item.title}
+						</Text>
+
+						{item.content && (
+							<View style={{ rowGap: 20 }}>
+								{item.content.map((content, index) => (
+									<Text
+										style={{ color: Colors.textColor + '9a', lineHeight: 20 }}
+										key={index}
+									>
+										{content}
+									</Text>
+								))}
+							</View>
+						)}
+
+						{item.list_items && (
+							<View style={{ rowGap: 8, marginTop: 8 }}>
+								{item?.list_items.map((content, index) => (
+									<View
+										key={index}
+										style={{
+											flexDirection: 'row',
+											columnGap: 8,
+											marginLeft: 8,
+											paddingRight: 20
+										}}
+									>
+										<Circle
+											strokeWidth={0}
+											fill={Colors.textColor}
+											style={{ marginTop: 7 }}
+											size={6}
+										/>
+										<Text style={{ color: Colors.textColor + '9a', lineHeight: 20 }}>
+											{content}
+										</Text>
+									</View>
+								))}
+							</View>
+						)}
+
+						{item.additional_content && (
+							<View style={{ rowGap: 20, marginTop: 20 }}>
+								{item.additional_content.map((content, index) => (
+									<Text
+										style={{ color: Colors.textColor + '9a', lineHeight: 20 }}
+										key={index}
+									>
+										{content}
+									</Text>
+								))}
+							</View>
+						)}
+
+						{item.subsections && (
+							<View style={{ rowGap: 20, marginTop: 20 }}>
+								{item.subsections.map(
+									({ title, content, list_items, additional_content }, index) => (
+										<View style={{ rowGap: 4 }} key={index}>
+											<Text style={{ color: Colors.textColor, fontWeight: 700 }}>
+												{title}
+											</Text>
+
+											<Text style={{ color: Colors.textColor }} key={index}>
+												{content}
+											</Text>
+
+											{list_items.map((item) => (
+												<View
+													key={index + item}
+													style={{
+														flexDirection: 'row',
+														columnGap: 8,
+														marginLeft: 8,
+														paddingRight: 20
+													}}
+												>
+													<Circle
+														strokeWidth={0}
+														fill={Colors.textColor}
+														style={{ marginTop: 7 }}
+														size={6}
+													/>
+													<Text
+														style={{ color: Colors.textColor + '9a', lineHeight: 20 }}
+													>
+														{item}
+													</Text>
+												</View>
+											))}
+
+											{additional_content && (
+												<Text style={{ color: Colors.textColor + '9a' }}>
+													{additional_content}
+												</Text>
+											)}
+										</View>
+									)
+								)}
+							</View>
+						)}
+					</View>
+				))}
+			</ScrollView>
+		</>
+	);
+}
--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,8 +1,5 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { Pressable, View } from 'react-native';
-import { createAnimatedComponent } from 'react-native-reanimated';
-
-const AnimatedPressable = createAnimatedComponent(Pressable);
+import Colors from '@/constants/Colors';
+import { TouchableOpacity, View } from 'react-native';
 
 export default function PrimaryButton({
 	children,
@@ -12,9 +9,12 @@ export default function PrimaryButton({
 	disabled
 }) {
 	return (
-		<AnimatedPressable
+		<TouchableOpacity
+			activeOpacity={0.7}
 			style={[
 				{
+					backgroundColor: Colors.primary,
+					padding: 16,
 					borderRadius: 24,
 					overflow: 'hidden',
 					shadowColor: '#00000052',
@@ -25,37 +25,26 @@ export default function PrimaryButton({
 					shadowOpacity: 0.17,
 					shadowRadius: 3.05,
 					elevation: 4,
-					opacity: disabled ? 0.45 : 1,
-					transitionDuration: 400
+					opacity: disabled ? 0.3 : 1
 				},
 				{ ...containerStyle }
 			]}
 			disabled={disabled}
-			android_ripple={{ color: '#e0dada6a', foreground: true }}
 			onPress={handlePress}
 		>
-			<LinearGradient
-				start={{ x: 0.3, y: 0.7 }}
-				end={{ x: 1, y: 0.2 }}
-				colors={['#b8a4f5', '#ffb9ca']}
-				style={{
-					padding: 16
-				}}
+			<View
+				style={[
+					{
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'center',
+						alignItems: 'center'
+					},
+					{ ...styles }
+				]}
 			>
-				<View
-					style={[
-						{
-							display: 'flex',
-							flexDirection: 'row',
-							justifyContent: 'center',
-							alignItems: 'center'
-						},
-						{ ...styles }
-					]}
-				>
-					{children}
-				</View>
-			</LinearGradient>
-		</AnimatedPressable>
+				{children}
+			</View>
+		</TouchableOpacity>
 	);
 }

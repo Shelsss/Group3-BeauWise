806cdfe5f6f7327d00269a59c00caf823d53df8c
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-04-06 18:57:55 +0800

    Add new components for the authentication

---
+++ src/components/CreateAccountButton.jsx
@@ -0,0 +1,48 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import Colors from '@/constants/Colors';
+import { ArrowRight } from 'lucide-react-native';
+import { router } from 'expo-router';
+export default function CreateAccountButton() {
+	return (
+		<TouchableOpacity
+			onPress={() => router.push('authentication/sign-up')}
+			activeOpacity={0.7}
+			style={{
+				marginTop: 6,
+				paddingVertical: 14,
+				paddingHorizontal: 20,
+				flexDirection: 'row',
+				alignItems: 'center',
+				backgroundColor: Colors.primary,
+				padding: 16,
+				borderRadius: 16
+			}}
+		>
+			<View>
+				<Text
+					style={{
+						fontWeight: 600,
+						color: Colors.backgroundColor
+					}}
+				>
+					Create Your Account
+				</Text>
+				<Text
+					style={{
+						fontSize: 12,
+						color: Colors.backgroundColor,
+						marginLeft: 'auto'
+					}}
+				>
+					Start tracking your beauty journey
+				</Text>
+			</View>
+
+			<ArrowRight
+				style={{ marginLeft: 'auto' }}
+				color={Colors.backgroundColor}
+				size={18}
+			/>
+		</TouchableOpacity>
+	);
+}
---
+++ src/components/Input.jsx
@@ -0,0 +1,122 @@
+import Colors from '@/constants/Colors';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Eye, EyeOff } from 'lucide-react-native';
+import Animated, {
+	useAnimatedStyle,
+	useSharedValue,
+	withRepeat,
+	withSequence,
+	withTiming
+} from 'react-native-reanimated';
+export default function Input({
+	children,
+	isPassword = false,
+	contentType,
+	placeholder,
+	onChangeText,
+	value,
+	error
+}) {
+	const opacity = useSharedValue(0);
+	const positionX = useSharedValue(1);
+	const [passwordVisibility, setPasswordVisibility] = useState(false);
+
+	const handlePasswordVisibility = () => {
+		setPasswordVisibility(!passwordVisibility);
+	};
+
+	const animatedStyle = useAnimatedStyle(() => {
+		return {
+			opacity: opacity.value,
+			transform: [{ translateX: positionX.value }]
+		};
+	});
+	const OFFSET = 5;
+	const TIME = 50;
+
+	useEffect(() => {
+		opacity.value = withTiming(1, { duration: 200 });
+		positionX.value = withSequence(
+			withTiming(-OFFSET, { duration: TIME / 2 }),
+			withRepeat(withTiming(OFFSET, { duration: TIME }), 4, true),
+			withTiming(0, { duration: TIME / 2 })
+		);
+	}, [error]);
+
+	return (
+		<View>
+			<View
+				style={{
+					backgroundColor: Colors.backgroundColor,
+					flexDirection: 'row',
+					alignItems: 'center',
+					paddingVertical: 8,
+					paddingHorizontal: 20,
+					borderRadius: 16,
+					columnGap: 6,
+					shadowColor: '#000000a3',
+					shadowOffset: {
+						width: 0,
+						height: 1
+					},
+					shadowOpacity: 0.2,
+					shadowRadius: 1.41,
+
+					elevation: 2,
+					overflow: 'hidden'
+				}}
+			>
+				{children}
+				<TextInput
+					value={value}
+					onChangeText={onChangeText}
+					style={{ flex: 1 }}
+					placeholder={placeholder}
+					autoComplete={contentType}
+					secureTextEntry={isPassword ? !passwordVisibility : false}
+					autoCapitalize='none'
+					textContentType={contentType}
+				/>
+
+				{isPassword && (
+					<View>
+						{passwordVisibility ? (
+							<TouchableOpacity
+								style={{ padding: 4, paddingVertical: 8 }}
+								onPress={handlePasswordVisibility}
+							>
+								<Eye color={Colors.textColor + '7a'} size={16} />
+							</TouchableOpacity>
+						) : (
+							<TouchableOpacity
+								style={{ padding: 4, paddingVertical: 8 }}
+								onPress={handlePasswordVisibility}
+							>
+								<EyeOff color={Colors.textColor + '7a'} size={16} />
+							</TouchableOpacity>
+						)}
+					</View>
+				)}
+
+				<Animated.Text
+					style={[
+						animatedStyle,
+						{
+							bottom: 2,
+							left: 20,
+							fontSize: 8,
+							position: 'absolute',
+							marginLeft: 4,
+							color: '#ff6565',
+							fontWeight: 500,
+							letterSpacing: 0.7
+						}
+					]}
+				>
+					{error?.message}
+				</Animated.Text>
+			</View>
+		</View>
+	);
+}

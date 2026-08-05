73307df2783057a968f4bc5f61c85ea998568da0
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-08-05 12:53:47 +0800

    Update fda layout

--- src/app/fda/_layout.jsx
+++ src/app/fda/_layout.jsx
@@ -1,10 +1,19 @@
+import styles from '@/config/styles';
+import { useThemeStore } from '@/stores/useThemeStore';
 import { Stack } from 'expo-router';
+import { useColorScheme } from 'react-native';
 
 export default function BatchLayout() {
+	const systemTheme = useColorScheme() ?? 'light';
+	const themeMode = useThemeStore((state) => state.themeMode);
+	const activeTheme = themeMode === 'system' ? systemTheme : themeMode;
 	return (
 		<Stack
 			screenOptions={{
-				headerShown: false
+				headerShown: false,
+				contentStyle: {
+					backgroundColor: styles.theme.colors[activeTheme].screen_background
+				}
 			}}
 		/>
 	);

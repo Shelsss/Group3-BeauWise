151c1789fed5c7cd0522f65c7e0114baba1b5549
navi-cc <157870465+navi-cc@users.noreply.github.com>
2026-08-05 13:14:02 +0800

    Update profiling layout

--- src/app/profiling/_layout.jsx
+++ src/app/profiling/_layout.jsx
@@ -1,21 +1,28 @@
 import AlbumImage from '@/components/icons/hugeicons/AlbumImage';
 import ProfilingFooter from '@/components/profiling/ProfilingFooter';
 import ProfilingHeader from '@/components/profiling/ProfilingHeader';
-import Colors from '@/constants/Colors';
+import styles from '@/config/styles';
 import Questionnaire from '@/constants/Questionnaire';
 import { useProfilingStore } from '@/stores/useProfilingStore';
+import { useThemeStore } from '@/stores/useThemeStore';
 import { Image } from 'expo-image';
 
-import { Slot, useGlobalSearchParams, useSegments } from 'expo-router';
+import { router, Slot, useGlobalSearchParams, useSegments } from 'expo-router';
 import LottieView from 'lottie-react-native';
 import { X } from 'lucide-react-native';
 import { useEffect, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { BackHandler, Text, TouchableOpacity, useColorScheme, View } from 'react-native';
 import { Modal, Portal } from 'react-native-paper';
 import { ResumableZoom } from 'react-native-zoom-toolkit';
 import { useDebouncedCallback } from 'use-debounce';
 
+const holdGesture = require('assets/lottie/touch-hold-gesture.json');
+const holdGestureWhite = require('assets/lottie/touch-hold-gesture-white.json');
 export default function ProfilingLayout() {
+	const systemTheme = useColorScheme() ?? 'light';
+	const themeMode = useThemeStore((state) => state.themeMode);
+	const activeTheme = themeMode === 'system' ? systemTheme : themeMode;
+
 	const params = useGlobalSearchParams();
 	const segments = useSegments();
 
@@ -29,6 +36,8 @@ export default function ProfilingLayout() {
 	const showProfileZoom = useProfilingStore((state) => state.showProfileZoom);
 	const setShowProfileZoom = useProfilingStore((state) => state.setShowProfileZoom);
 
+	const setSlideDirection = useProfilingStore((state) => state.setSlideDirection);
+
 	const imageZoomSrc = useProfilingStore((state) => state.imageZoomSrc);
 	const hideProfileZoom = () => setShowProfileZoom(!showProfileZoom);
 
@@ -38,9 +47,29 @@ export default function ProfilingLayout() {
 	const hideModal = () => setVisible(false);
 	const debounceShowModal = useDebouncedCallback(showModal, 280);
 
+	useEffect(() => {
+		const backAction = () => {
+			setSlideDirection('backward');
+
+			if (!router.canGoBack()) {
+				return false;
+			}
+
+			router.back();
+			return true;
+		};
+
+		const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+		return () => backHandler.remove();
+	}, []);
 	return (
 		<>
-			<ProfilingHeader isTransition={isTransition} currentStep={currentStep} />
+			<ProfilingHeader
+				setSlideDirection={setSlideDirection}
+				isTransition={isTransition}
+				currentStep={currentStep}
+			/>
 			<Slot />
 			<ProfilingFooter
 				isTransition={isTransition}
@@ -77,34 +106,56 @@ export default function ProfilingLayout() {
 						}}
 						activeOpacity={0.7}
 					>
-						<X color={'#fff'} />
+						<X color={styles.icon.colors._05} />
 					</TouchableOpacity>
 					<Text
 						style={{
 							position: 'absolute',
-							fontFamily: 'Outfit',
-							color: '#fff',
+							fontFamily: styles.font.family,
+							color: styles.font.colors._04,
 							alignSelf: 'center',
 							transform: [{ translateY: -190 }],
-							fontWeight: 500
+							fontWeight: styles.font.weight.light
 						}}
 					>
 						Double-tap or pinch to zoom the image
 					</Text>
 
-					<Text
+					<View
 						style={{
-							position: 'absolute',
-							fontFamily: 'Outfit',
-							color: '#fff',
 							alignSelf: 'center',
-							transform: [{ translateY: 200 }],
-							fontWeight: 500,
-							fontSize: 16
+							transform: [{ translateY: 210 }],
+							position: 'absolute',
+							alignItems: 'center',
+							rowGap: styles.spacing.md
 						}}
 					>
-						{imageZoomSrc?.label}
-					</Text>
+						<Text
+							style={{
+								fontFamily: styles.font.family,
+								color: styles.font.colors._04,
+								fontWeight: styles.font.weight.light,
+								fontSize: styles.font.size.lg
+							}}
+						>
+							{imageZoomSrc?.label}
+						</Text>
+
+						{imageZoomSrc?.description && (
+							<Text
+								style={{
+									fontFamily: styles.font.family,
+									color: styles.font.colors._04,
+
+									fontWeight: styles.font.weight.light,
+									fontSize: styles.font.size.lg
+								}}
+							>
+								{imageZoomSrc.description}
+							</Text>
+						)}
+					</View>
+
 					<ResumableZoom
 						style={{
 							backgroundColor: 'transparent'
@@ -116,7 +167,9 @@ export default function ProfilingLayout() {
 							style={{
 								borderRadius: 20,
 
-								backgroundColor: Colors.backgroundColor,
+								backgroundColor: styles.theme.colors[activeTheme].card_background,
+								borderWidth: 1,
+								borderColor: styles.theme.colors[activeTheme].card_border,
 								aspectRatio: 1,
 								width: 320
 							}}
@@ -135,20 +188,27 @@ export default function ProfilingLayout() {
 					<View
 						style={{
 							padding: 20,
-							backgroundColor: Colors.backgroundColor,
-							borderRadius: 20,
+							backgroundColor: styles.theme.colors[activeTheme].card_background,
+							borderWidth: 1,
+							borderColor: styles.theme.colors[activeTheme].card_border,
+							borderRadius: styles.border.radius.size.md,
 							alignItems: 'center',
 							rowGap: 30
 						}}
 					>
-						<Text style={{ fontFamily: 'Outfit', color: Colors.textColor }}>
+						<Text
+							style={{
+								fontFamily: styles.font.family,
+								color: styles.theme.colors[activeTheme].text
+							}}
+						>
 							Hold the option to view the image clearly
 						</Text>
 
 						<View
 							style={{
 								borderWidth: 1,
-								borderColor: '#3341554a',
+								borderColor: styles.theme.colors[activeTheme].card_border,
 								borderRadius: 14,
 								aspectRatio: 9 / 16,
 
@@ -157,23 +217,23 @@ export default function ProfilingLayout() {
 								justifyContent: 'center'
 							}}
 						>
-							<AlbumImage size={80} color='#334155' />
+							<AlbumImage size={80} color={styles.theme.colors[activeTheme].icon} />
 						</View>
 
 						<TouchableOpacity
 							activeOpacity={0.7}
 							onPress={hideModal}
 							style={{
-								backgroundColor: Colors.primary,
+								backgroundColor: styles.theme.colors.primary,
 								marginTop: 14,
 								paddingVertical: 10,
 								paddingHorizontal: 40,
-								borderRadius: 40
+								borderRadius: styles.border.radius.size.sm
 							}}
 						>
 							<Text
 								style={{
-									fontFamily: 'Outfit',
+									fontFamily: styles.font.family,
 									color: '#fff'
 								}}
 							>
@@ -184,12 +244,12 @@ export default function ProfilingLayout() {
 
 					<LottieView
 						autoPlay={true}
-						source={require('assets/lottie/touch-hold-gesture.json')}
+						source={activeTheme === 'light' ? holdGesture : holdGestureWhite}
 						style={{
 							position: 'absolute',
 							transform: [{ translateY: 50 }, { translateX: 40 }],
 							aspectRatio: 1,
-							width: 120
+							width: 80
 						}}
 					/>
 				</Modal>
